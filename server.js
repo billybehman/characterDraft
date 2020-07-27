97e9238270c31422ff864cbd348e8566c7458d5b
Billy Behman <billybehman@gmail.com>
2020-07-26 22:45:56 -0400

    heroku push

--- server.js
+++ server.js
@@ -25,9 +25,11 @@ app.use(session({
 }))
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("./client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("./client/build"));
+// }
+
+app.use(express.static(path.join(__dirname, "./client/build")))
 
 // mongodb:user1password1@ds125871.mlab.com:25871/heroku_0xn0jnk7
 
@@ -39,8 +41,8 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
 require("./routes/apiRoutes")(app);
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/public/index.html"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/", "index.html"));
 });
 
 app.listen(PORT, () => {
